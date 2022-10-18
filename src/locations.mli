ced79d166116c56319a15bca9fd0957931baaa32
KitilCode <nk528@cornell.edu>
2022-10-18 17:33:03 -0400

    Debugging Test Suite

--- src/locations.mli
+++ src/locations.mli
@@ -2,5 +2,5 @@ type property
 type tile_type
 
 val to_json : string -> Yojson.Basic.t
-val contents : Yojson.Basic.t -> Yojson.Basic.t
+val loc_contents : Yojson.Basic.t -> Yojson.Basic.t
 val property_record : Yojson.Basic.t -> property
