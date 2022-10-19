4269240c1afe5e8c1554347b72d03b88e0ce9187
John Aclufi <ja525@cornell.edu>
2022-10-19 18:08:09 -0400

    Added psuedo code of moving player w/ dice roll idea

--- src/board.ml
+++ src/board.ml
@@ -9,6 +9,14 @@
    utilizing built-in Random module, reseeds the generator with the current UNIX
    time.*)
 (* let roll_dice () = let _ = Random.init (int_of_float (Unix.gettimeofday ()))
-   in Random.int 5 + 1
+   in Random.int 5 + 1 *)
 
-   let dice1 = roll_dice () let dice2 = roll_dice () *)
+(** [do_turn player] takes a player type and returns a new player with the
+    updated board position after a dice roll *)
+(* let do_turn (player : Player.player) : Player.player = (* before this we
+   should output the player's turn has begun *) let dice1 = roll_dice () in let
+   dice2 = roll_dice () in let total_movement = dice1 + dice2 in (* before this
+   we should output the player's invidiual dice rolls & then increment their
+   position *) let board_position = (get_board_position player) + total_movement
+   in (* here we should communicate the new tile the player is on & handle any
+   effects of landing on that new tile *) *)
--- src/locations.mli
+++ src/locations.mli
@@ -10,7 +10,7 @@ val parse_tile_type : Yojson.Basic.t -> tile_type
     record, and returns tile_type. Requires: a valid tile*)
 
 val tiles_list : Yojson.Basic.t -> (int * tile_type) list
-(** [tiles_list json] takes json represnetations of a monopoly board and returns
+(** [tiles_list json] takes json representations of a monopoly board and returns
     a list of locations where each tile is assoicated with its index*)
 
 val find_tile : int -> (int * tile_type) list -> tile_type
