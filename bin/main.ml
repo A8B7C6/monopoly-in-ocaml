247b70d0cb82854952297741aceeb3963e151e71
Savitta Sivapalan <ss2849@cornell.edu>
2022-10-23 20:12:39 -0400

    Edited play_monopoly, do_turn functions
    -Implemented a loop in play_monopoly() to continuously roll dice
    -Fixed bug in do_turn function that incorrectly performed mod operator

--- bin/main.ml
+++ bin/main.ml
@@ -7,20 +7,29 @@ let do_turn (player : _player) =
   let dice_roll = roll_dice () in
   print_endline ("You rolled a " ^ (dice_roll |> string_of_int));
   let new_pos = cur + dice_roll in
-  if new_pos > 39 then player.board_position <- new_pos - 39
+  if new_pos > 39 then player.board_position <- (new_pos mod 39) - 1
   else player.board_position <- new_pos
 
-let play_monopoly player =
+let curr_pos_print player state =
   let p1name = player.name in
   print_endline
-    ("\n" ^ p1name ^ ", your current position is "
+    ("\n" ^ p1name ^ ", your " ^ state ^ " position is "
     ^ (player |> get_board_position |> string_of_int)
-    ^ "\n");
+    ^ "\n")
 
-  do_turn player;
-  print_endline
-    (p1name ^ ", your new board position is "
-    ^ (player |> get_board_position |> string_of_int))
+let play_monopoly player =
+  curr_pos_print player "current";
+  let rec play_loop continue =
+    match continue with
+    | "y" ->
+        do_turn player;
+        curr_pos_print player "new";
+        print_endline "Continue playing? y/n";
+        let cont = read_line () in
+        play_loop cont
+    | _ -> exit 0
+  in
+  play_loop "y"
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
