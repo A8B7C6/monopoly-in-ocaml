16b07dbbb83fc3ebd427ffd877b395b32ae3084c
John Aclufi <ja525@cornell.edu>
2022-11-16 18:18:20 -0500

    Start of ASCII board & add helper string parsing functions

--- src/board.ml
+++ src/board.ml
@@ -16,6 +16,58 @@ let do_turn player =
     ("You rolled a " ^ string_of_int frst ^ " and a " ^ string_of_int scnd);
   { player with board_position = (player.board_position + frst + scnd) mod 40 }
 
+let multiply_str str num =
+  let rec helper str num final =
+    if num = 0 then final else helper str (num - 1) (final ^ str)
+  in
+  helper str num ""
+
+let adjust_str str target_length =
+  if target_length - String.length str > 0 then
+    str ^ multiply_str " " (target_length - String.length str)
+  else String.sub str 0 target_length
+
+let print_border () = print_endline (multiply_str "+---------------+" 11)
+
+let print_row fst last top_row =
+  let rec helper fst last final =
+    let str =
+      if top_row then " Tile: " ^ string_of_int fst
+      else
+        let tile_no = last - 10 + (last - fst) in
+        if tile_no = 40 then " Tile: " ^ string_of_int 0
+        else " Tile: " ^ string_of_int tile_no
+    in
+    if fst > last then final
+    else final ^ adjust_str str 15 ^ "|" ^ helper (fst + 1) last final
+  in
+  print_endline (helper fst last "|")
+
+let print_side_rows () =
+  let rec helper pos rpos =
+    let str_left = "| Tile: " ^ string_of_int pos in
+    let str_right = "| Tile: " ^ string_of_int rpos in
+    print_endline
+      (adjust_str str_left 15 ^ " ||" ^ multiply_str " " 151 ^ "|"
+     ^ adjust_str str_right 15 ^ " ||");
+    print_endline
+      (adjust_str "| " 15 ^ " ||" ^ multiply_str " " 151 ^ "|"
+     ^ adjust_str "| " 15 ^ " ||");
+    print_endline
+      (adjust_str "|_________________" 16
+      ^ "||" ^ multiply_str " " 151 ^ "|"
+      ^ adjust_str "|_________________" 16
+      ^ "||");
+    if pos = 1 then () else helper (pos - 1) (rpos + 1)
+  in
+  helper 9 21
+
+let print_board () =
+  print_border ();
+  print_row 10 20 true;
+  print_side_rows ();
+  print_row 30 40 false;
+  print_border ()
 (*let info_position player = let tilenm = find_tile (player.board_position)
   monopoly in *)
 
--- src/board.mli
+++ src/board.mli
@@ -1,7 +1,8 @@
 open Player
 
 val roll_dice : unit -> int
-
 val do_turn : _player -> _player
+
+val print_board : unit -> unit
 (**takes a player type and returns a new player with the updated board position
    after a dice roll *)
