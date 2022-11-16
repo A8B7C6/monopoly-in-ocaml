bba3f3ba3684d75e9fd95f7c045087d90b9e5d8f
Kitil <nk528@cornell.edu>
2022-11-16 08:39:06 -0500

    Cards (#3)
    * Debugging architecture issues w/ Graphics library
    
    * Implemnting Cards Funcs that don't Effect State
    
    - Implemented type card (only handles name and flavor txt)
    - Implemented .init_card (only handles name and flavor txt)
    - Implemented .to_json .card_name .card_flvr_txt
    - Implemented .card_display_info
    - Changed where name of a card can be found in Cards.json
    
    * Debugging architecture issues w/ Graphics library
    
    * Implemnting Cards Funcs that don't Effect State
    
    - Implemented type card (only handles name and flavor txt)
    - Implemented .init_card (only handles name and flavor txt)
    - Implemented .to_json .card_name .card_flvr_txt
    - Implemented .card_display_info
    - Changed where name of a card can be found in Cards.json
    
    * Removing Operations Associated with GUI
    
    Co-authored-by: John Aclufi <ja525@cornell.edu>

--- src/cards.ml
+++ src/cards.ml
@@ -1,4 +1,23 @@
+open Yojson.Basic.Util
+
+(* source material: https://monopoly.fandom.com/wiki/Community_Chests*)
 type card = {
   name : string;
   flavor_text : string;
 }
+
+type t = Yojson.Basic.t
+
+let init_card nm flvr_txt = { name = nm; flavor_text = flvr_txt }
+
+(* FIX : think we should move this function so we dont have it written twice, as
+   of rn is written once here and once in locations *)
+let to_json json = Yojson.Basic.from_file json
+let card_name j = j |> fst |> to_string
+let card_flvr_txt j = j |> snd |> member "flavor text" |> to_string
+
+(* BUILD : need to build card_list, have started it: let card_list j = let data
+   = to_assoc j in data*)
+
+let card_display_info crd =
+  "Picked up card " ^ crd.name ^ ": " ^ crd.flavor_text
