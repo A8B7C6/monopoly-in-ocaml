ae067471a251326ee8022dd26cde38cac236b57d
Kitil <nk528@cornell.edu>
2022-11-24 10:09:47 -0500

    Deleting CLI GUI Functions from Board

--- src/board.mli
+++ src/board.mli
@@ -3,6 +3,5 @@ open Player
 val roll_dice : unit -> int
 val do_turn : _player -> _player
 
-val print_board : unit -> unit
 (**takes a player type and returns a new player with the updated board position
    after a dice roll *)
